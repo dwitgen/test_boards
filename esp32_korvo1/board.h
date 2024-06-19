0eb7ed1f283a032e72ddf50172a34badb3894647
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-19 10:05:52 -0400

    Update board.h

--- esp32_korvo1/board.h
+++ esp32_korvo1/board.h
@@ -135,7 +135,8 @@ audio_board_handle_t audio_board_get_handle(void);
 esp_err_t audio_board_deinit(audio_board_handle_t audio_board);
 
 // Function i2s config
-esp_err_t configure_resample_filter(audio_element_handle_t *filter);
+esp_err_t configure_resample_filter_http(audio_element_handle_t *filter);
+esp_err_t configure_resample_filter_raw(audio_element_handle_t *filter);
 esp_err_t configure_i2s_stream_writer_http(audio_element_handle_t *i2s_stream_writer);
 esp_err_t configure_i2s_stream_writer_raw(audio_element_handle_t *i2s_stream_writer);
 
