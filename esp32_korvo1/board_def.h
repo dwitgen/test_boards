3d3ec81c58872a7961f1d7993acaea8798766fcb
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-21 10:55:46 -0400

    Update board_def.h

--- esp32_korvo1/board_def.h
+++ esp32_korvo1/board_def.h
@@ -125,7 +125,7 @@ extern audio_hal_func_t AUDIO_CODEC_ES7210_DEFAULT_HANDLE;
 #define BUTTON_MODE_ID              4
 #define BUTTON_REC_ID               1
 
-#define INPUT_KEY_DEFAULT_INFO() {                      \
+/*#define INPUT_KEY_DEFAULT_INFO() {                      \
      {                                                  \
         .type = PERIPH_ID_ADC_BTN,                      \
         .user_id = INPUT_KEY_USER_ID_REC,               \
@@ -156,6 +156,6 @@ extern audio_hal_func_t AUDIO_CODEC_ES7210_DEFAULT_HANDLE;
         .user_id = INPUT_KEY_USER_ID_VOLDOWN,           \
         .act_id = BUTTON_VOLDOWN_ID,                    \
     }                                                   \
-}
+}*/
 
 #endif
